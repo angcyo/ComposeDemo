991821b206a4d5bdbea045130d4b2cc0cba50198
angcyo <angcyo@126.com>
2025-03-31 18:09:03 +0800

    init

--- app/src/main/java/com/angcyo/compose/demo/ui/theme/Theme.kt
+++ app/src/main/java/com/angcyo/compose/demo/ui/theme/Theme.kt
@@ -3,7 +3,9 @@ package com.angcyo.compose.demo.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -33,6 +35,12 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+/**
+ * [MaterialTheme]
+ * - [ColorScheme]
+ * - [Typography]
+ * - [Shapes]
+ * */
 @Composable
 fun ComposeDemoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
