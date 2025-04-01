ef748b9c7ce506954395f6f4d9fcc7d857441e78
angcyo <angcyo@126.com>
2025-04-01 17:58:09 +0800

    init

--- app/src/main/java/com/angcyo/compose/demo/App.kt
+++ app/src/main/java/com/angcyo/compose/demo/App.kt
@@ -3,6 +3,7 @@ package com.angcyo.compose.demo
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
@@ -13,12 +14,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.offset
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -58,6 +60,17 @@ fun App() {
             var clicks by remember { mutableIntStateOf(0) }
             ClickCounter(clicks) {
                 clicks++
+            }
+            //--
+            Box(modifier = Modifier.layout { measurable, constraints ->
+                val padding = 10
+                val placeable = measurable.measure(constraints.offset(vertical = -padding))
+                val placeable2 = measurable.measure(constraints.offset(vertical = -padding * 2))
+                layout(placeable.width, placeable2.height + padding) {
+                    placeable2.placeRelative(0, padding)
+                }
+            }) {
+                Text("Layout Box")
             }
         }
     }
