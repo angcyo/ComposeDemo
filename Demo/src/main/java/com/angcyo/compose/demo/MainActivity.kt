945c9c3ec7fc5deec92854e3f50297e5c5e05796
angcyo <angcyo@126.com>
2025-11-08 18:13:52 +0800

    init

--- Demo/src/main/java/com/angcyo/compose/demo/MainActivity.kt
+++ Demo/src/main/java/com/angcyo/compose/demo/MainActivity.kt
@@ -2,14 +2,17 @@ package com.angcyo.compose.demo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,8 +24,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import com.angcyo.compose.basics.NotificationHelper
+import com.angcyo.compose.basics.toast
+import com.angcyo.compose.demo.services.KeepAliveService
 import com.angcyo.compose.demo.ui.theme.ComposeDemoTheme
 
 /**
@@ -83,10 +90,41 @@ enum class AppDestinations(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val activity = LocalActivity.current
+    val context = LocalContext.current
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(onClick = {
+            if (!NotificationHelper.isNotificationEnabled(context)) {
+                //toast 提示
+                context.toast("请打开通知权限")
+                NotificationHelper.requestNotificationPermission(activity)
+            }
+        }) {
+            Text(text = "打开通知权限")
+        }
+        Button(onClick = {
+            if (!NotificationHelper.isChannelEnabled(context, KeepAliveService.CHANNEL_ID)) {
+                //toast 提示
+                context.toast("请打开通知通道")
+                NotificationHelper.openNotificationChannelSettings(
+                    activity,
+                    KeepAliveService.CHANNEL_ID
+                )
+            }
+        }) {
+            Text(text = "打开通道通知")
+        }
+        Button(onClick = {
+            KeepAliveService.start(context)
+        }) {
+            Text(text = "启动保活服务")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
---
+++ Demo/src/main/java/com/angcyo/compose/demo/services/KeepAliveService.kt
@@ -0,0 +1,107 @@
+package com.angcyo.compose.demo.services
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import com.angcyo.compose.basics.NotificationHelper
+import com.angcyo.compose.demo.MainActivity
+
+/**
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @date 2025/11/08
+ *
+ * 保活服务
+ *
+ * ## RuntimeException
+ *
+ * ```
+ * java.lang.RuntimeException: Unable to create service com.angcyo.compose.demo.services.KeepAliveService: java.lang.SecurityException: Permission Denial: startForeground from pid=16397, uid=10279 requires android.permission.FOREGROUND_SERVICE
+ *
+ * ...
+ *
+ * <uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
+ * <uses-permission android:name="android.permission.FOREGROUND_SERVICE_DATA_SYNC" />
+ *
+ * ```
+ *
+ * ## RuntimeException
+ *
+ * ```
+ * java.lang.RuntimeException: Unable to create service com.angcyo.compose.demo.services.KeepAliveService: android.app.MissingForegroundServiceTypeException: Starting FGS without a type  callerApp=ProcessRecord{7a11ffd 16919:com.angcyo.compose.demo/u0a279} targetSDK=36
+ *
+ * ...
+ * <uses-permission android:name="android.permission.FOREGROUND_SERVICE_DATA_SYNC" />
+ * <service
+ *             android:name=".services.KeepAliveService"
+ *             android:foregroundServiceType="dataSync" />
+ * ```
+ */
+class KeepAliveService : Service() {
+
+    companion object {
+        const val CHANNEL_ID = "keep_alive_channel"
+        const val CHANNEL_NAME = "keep_alive"
+        const val NOTIFY_ID = 1001
+
+        /**启动*/
+        fun start(context: Context) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(Intent(context, KeepAliveService::class.java))
+            } else {
+                context.startService(Intent(context, KeepAliveService::class.java))
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationHelper.createNotificationChannel(this, CHANNEL_ID, CHANNEL_NAME)
+        }
+        // Android O+ use startForegroundService to start; must call startForeground soon after
+        startForeground(NOTIFY_ID, showNotification())
+    }
+
+    /**
+     * - [START_STICKY]
+     * - [START_NOT_STICKY]
+     * - [START_REDELIVER_INTENT]
+     * */
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // 做必要的心跳/检查/任务
+        if (startId > 1) {//第几次启动
+            showNotification()
+        }
+        return START_STICKY // 被系统杀后可能会尝试重启服务
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    /**显示一个通知*/
+    fun showNotification(): Notification {
+        return NotificationHelper.showNotification(
+            this,
+            CHANNEL_ID,
+            NOTIFY_ID,
+            "App 正在运行",
+            "保活服务已启动",
+            PendingIntent.getActivity(
+                this,
+                0,
+                Intent(
+                    this,
+                    MainActivity::class.java
+                ),
+                PendingIntent.FLAG_IMMUTABLE
+            )
+        )
+    }
+}
