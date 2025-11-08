8b57453ed65f092243354973f4d9c8cb3ec4f99c
angcyo <angcyo@126.com>
2025-11-08 15:36:14 +0800

    init

--- Demo/src/main/java/com/angcyo/compose/demo/MainActivity.kt
+++ Demo/src/main/java/com/angcyo/compose/demo/MainActivity.kt
@@ -25,6 +25,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.angcyo.compose.demo.ui.theme.ComposeDemoTheme
 
+/**
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @date 2025/11/08
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
