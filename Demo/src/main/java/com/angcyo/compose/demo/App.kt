9de65303faa0b15738a41a40e358db1659f1b4e0
angcyo <angcyo@126.com>
2025-11-11 11:38:27 +0800

    init app

---
+++ Demo/src/main/java/com/angcyo/compose/demo/App.kt
@@ -0,0 +1,10 @@
+package com.angcyo.compose.demo
+
+import com.angcyo.compose.app.CoreApplication
+
+/**
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @date 2025/11/11
+ */
+class App : CoreApplication() {
+}
